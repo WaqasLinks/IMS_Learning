1f5a528ad16da9673f484b4154df13a9788fd20c
Waqas Javaid <waqqasjavaid@gmail.com>
2022-01-01 22:46:26 +0500

    Add project files.

---
+++ IMS/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+[assembly: OwinStartup(typeof(IMS.Startup))]
+
+namespace IMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
