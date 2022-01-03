fc42339b2d5de6a0961e1eb39c98b3567bee9eba
Waqas Javaid <waqqasjavaid@gmail.com>
2022-01-03 11:20:46 +0500

    Class 2- added Customers Controller

---
+++ IMS/Controllers/CustomersController.cs
@@ -0,0 +1,29 @@
+﻿using IMSRepo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IMS.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        private IMSEntities db = new IMSEntities();
+        public ActionResult CustomersList()
+        {
+            List<Customer> customers = db.Customers.ToList();
+
+            return View(customers);
+        }
+        public ActionResult Update(string id)
+        {
+            Customer customer = db.Customers.Where(x => x.Id == id).FirstOrDefault();
+
+
+                //List<Customer> customer = db.Customers.Where(x => x.Id == id).ToList();
+                return View(customer);
+            }
+        }
+    }
